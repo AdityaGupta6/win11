238236b3931ae77cf4aab8073dbd01bdfb32810c
AdityaGupta6 <75517154+AdityaGupta6@users.noreply.github.com>
2021-07-03 14:30:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+var taskbar=document.getElementsByClassName("taskbar")[0]
+var startmenu=document.getElementsByClassName('startmenu')[0]
+
+
+taskbar.addEventListener("click",()=>{
+        if (startmenu.style.bottom==="50px") {
+            startmenu.style.bottom ="-650px"
+        } else {
+            startmenu.style.bottom="50px";
+        }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
